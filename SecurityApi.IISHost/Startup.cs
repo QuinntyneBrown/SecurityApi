ca42926b85c33dd11531c9975b92586b9a9017d5
Quinntyne Brown <qbrown@cetaris.com>
2015-10-22 18:17:29 -0400

    add IIS Host configuration

---
+++ SecurityApi.IISHost/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using Unity.WebApi;
+
+[assembly: OwinStartup(typeof(SecurityApi.IISHost.Startup))]
+
+namespace SecurityApi.IISHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(SecurityApi.UnityConfiguration.GetContainer());
+
+            GlobalConfiguration.Configure(config => SecurityApi.ApiConfiguration.Install(config, app));
+        }
+    }
+}
